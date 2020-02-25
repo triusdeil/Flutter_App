5a592b5556d5fd2ec0588f99224faf57decd687b
Triusdeil <34854558+triusdeil@users.noreply.github.com>
2020-02-25 19:54:06 +0100

    probando Scafford y Material App
    se hicieron algunos cambios al codigo como adiccion de una lista horizontal y con scroll

--- primera_app_flutter/lib/main.dart
+++ primera_app_flutter/lib/main.dart
@@ -2,10 +2,65 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    MyApp()
+    MyAppDos('otra tarea no mas')
   );
 }
-
+class MyAppDos extends StatefulWidget{
+ @override
+ String nuevaTarea;
+ MyAppDos(this.nuevaTarea);
+ State<MyAppDos> createState(){
+   return StateMiApp();
+ }
+}
+class StateMiApp extends State<MyAppDos>{
+  List<String> tareas;
+  @override
+  void initState() { 
+    super.initState();
+    print('build MiApp');
+     tareas = [
+    'Hacer el desayuno',
+    'Salir a caminar',
+    'Escribirle a ......a',
+    'Minecraft pa la madrugada con los compas'
+  ];
+  }
+ @override
+  Widget build(BuildContext context) {
+    print('build MiApp');
+    return  MaterialApp(
+     title:'MiApp',
+     home:Scaffold(
+       appBar: AppBar(
+         title: Text('Lista'),
+         backgroundColor: Colors.red,
+       ),//AppBar
+       body:Container(
+         height: 400.0,
+          child: ListView.builder(
+         scrollDirection: Axis.horizontal,
+         itemCount: tareas.length,
+         itemBuilder: (context,position){
+           return Tarea(tareas[position]);
+         },
+         //children: tareas.map( (elem) => Tarea(elem) ).toList()
+       )
+       ),
+         floatingActionButton: FloatingActionButton(
+           child: Icon(Icons.add),//Icono del Boton
+           backgroundColor: Colors.red,//Cambiar el color
+           onPressed:(){
+             setState(() {
+                    tareas.add(widget.nuevaTarea);
+                     print(tareas.length);
+             });
+             }
+         ),
+     ),//Scaffold 
+   );
+  }
+}
 class MyApp extends StatelessWidget{
   List<String> tareas = [
     'Hacer el desayuno',
@@ -21,13 +76,18 @@ class MyApp extends StatelessWidget{
      home:Scaffold(
        appBar: AppBar(
          title: Text('Lista'),
+         backgroundColor: Colors.teal,
        ),//AppBar
        body:Column(
          children: tareas.map( (elem) => Tarea(elem) ).toList()
        ),
          floatingActionButton: FloatingActionButton(
-           child: Icon(Icons.add),
-           onPressed:(){print('click');}
+           child: Icon(Icons.add),//Icono del Boton
+           backgroundColor: Colors.teal,//Cambiar el color
+           onPressed:(){
+              tareas.add('Nueva Tarea');
+              print(tareas.length);
+             }
          ),
      ),//Scaffold 
    );
@@ -40,10 +100,21 @@ class Tarea extends StatelessWidget{
   Tarea(this.tarea);
   @override 
   Widget build(BuildContext build){
-    return Card(
+    return GestureDetector(
+      child: Card(
       child: Center(
         child: Text(tarea),
       ),
+    ),
+    onTap: (){
+      print('click en la tarea $tarea');
+    },
+    onDoubleTap:(){
+      print('doble click en la tarea $tarea');
+    },
+    onLongPress: (){
+      print('Long click en la tarea $tarea');
+    }
     );
   }
 }
